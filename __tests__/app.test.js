3dd35041c3eff5de0ee008a29c13233d8aec483f
Emma Egstad <emma@egstad.com>
2022-03-15 17:56:55 -0500

    passing all tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Neopet = require('../lib/models/Neopet');
 
 describe('backend-anyapi routes', () => {
   beforeEach(() => {
@@ -10,5 +11,53 @@ describe('backend-anyapi routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a neopet', async () => {
+    const expected = {
+      name: 'Levi',
+      type: 'Gelert',
+      age: 8,
+      color: 'Black and White',
+    };
+    const res = await request(app).post('/api/v1/neopets').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of neopets', async () => {
+    const expected = await Neopet.findAll();
+    const res = await request(app).get('/api/v1/neopets');
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('gets a neopet by id', async () => {
+    const expected = await Neopet.findById(1);
+    const res = await request(app).get(`/api/v1/neopets/${expected.id}`);
+
+    expect(res.body).toEqual({ ...expected });
+  });
+
+  it('updates a neopet by id', async () => {
+    const expected = {
+      id: expect.any(String),
+      name: 'Michelle',
+      type: 'Jubjub',
+      age: 5,
+      color: 'White',
+    };
+    const res = await request(app)
+      .patch('/api/v1/neopets/1')
+      .send({ color: 'White' });
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('deletes a dog by id', async () => {
+    const expected = await Neopet.findById(1);
+    const res = await request(app).delete(`/api/v1/neopets/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS neopets;
+
+CREATE TABLE neopets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    age INT NOT NULL,
+    color TEXT NOT NULL
+);
+
+INSERT INTO
+    neopets (name, type, age, color)
+VALUES
+    ('Michelle', 'Jubjub', 5, 'Purple'),
+    ('Witch King of Angmar', 'Grundo', 700, 'Black');
